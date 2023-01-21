faf9f3661a1bcd8bc6949b20ae96245781b7f594
mleers <mleers4@gmail.com>
2023-01-21 14:03:35 -0800

    Initial Commit

---
+++ Rollit/RollitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollitApp.swift
+//  Rollit
+//
+//  Created by Mitch on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct RollitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
